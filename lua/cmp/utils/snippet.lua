7267add6c6f703bb4e897314e9dadc82754d2b46
Victor Oliveira <feroli@tuta.io>
2024-10-12 23:31:45 -0300

    Update snippet.lua

--- lua/cmp/utils/snippet.lua
+++ lua/cmp/utils/snippet.lua
@@ -406,7 +406,7 @@ M.NodeType = Node.Type
 function M.parse(input)
   local result = S.snippet(input, 1)
   if not result.parsed then
-    error('snippet parsing failed.')
+    return input
   end
   return result.value
 end
